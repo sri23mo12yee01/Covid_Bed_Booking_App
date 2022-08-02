353eb8c06b0ff3b92b9244d1ceabc6261f0707fe
unknown <srimoyee01dutta@gmail.com>
2022-08-02 17:47:24 +0530

    Initial commit

---
+++ CovidPatients-master/app/src/main/java/com/example/covid/model/hospitalModel.java
@@ -0,0 +1,75 @@
+package com.example.covid.model;
+
+public class hospitalModel
+{
+    String name,area,phone,email,total,id;
+    int vacant;
+
+    public hospitalModel() {
+    }
+
+    public hospitalModel(String name, String area,String phone, String email, String total, int vacant) {
+        this.name = name;
+        this.area = area;
+        this.phone = phone;
+        this.email = email;
+        this.total = total;
+        this.vacant = vacant;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public int getVacant() {
+        return vacant;
+    }
+
+    public void setVacant(int vacant) {
+        this.vacant = vacant;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
